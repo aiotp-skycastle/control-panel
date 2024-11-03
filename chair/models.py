d9a34a61ad183cb5794da17f229daae534af2df1
Youngho Cho <dominoyh5@gmail.com>
2024-11-03 16:16:24 +0900

    add: Chair model

--- chair/models.py
+++ chair/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Chair(models.Model):
+    datetime = models.DateTimeField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d %H:%M:%S') + ': ' + self.status
