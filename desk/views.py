004b8b439d764083d93c650db87479bd001528ee
Youngho Cho <dominoyh5@gmail.com>
2024-11-03 17:02:36 +0900

    add: desk call get api

--- desk/views.py
+++ desk/views.py
@@ -8,6 +8,17 @@ from .models import Desk
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CallView(View):
+    def get(self, request, *args, **kwargs):
+        latest_desk = Desk.objects.order_by('-datetime').first()
+        
+        if latest_desk:
+            return JsonResponse({
+                "success": True,
+                "datetime": latest_desk.datetime.strftime('%Y-%m-%d %H:%M:%S')
+            })
+        else:
+            return JsonResponse({"success": False, "error": "No entries found"}, status=404)
+
     def post(self, request, *args, **kwargs):
         desk = Desk(datetime=timezone.now())
         desk.save()
