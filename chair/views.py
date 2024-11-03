407ab4a99afef846ce8513b74f15c0e3a87db3c6
Youngho Cho <dominoyh5@gmail.com>
2024-11-03 16:21:36 +0900

    add: chair view

--- chair/views.py
+++ chair/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from django.utils import timezone
+
+from .models import Chair
+
+class ChairView(View):
+    def get(self, request, *args, **kwargs):
+        latest_chair = Chair.objects.order_by('-datetime').first()
+        
+        if latest_chair:
+            return JsonResponse({"success": True, "status": latest_chair.status, "datetime": latest_chair.datetime.strftime('%Y-%m-%d %H:%M:%S')})
+        else:
+            return JsonResponse({"success": False, "error": "No status found"}, status=404)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            status = data.get('status')
+            
+            if status not in ['on', 'yes']:
+                return JsonResponse({"success": False, "error": "Invalid status"}, status=400)
+            
+            chair = Chair(datetime=timezone.now(), status=status)
+            chair.save()
+            
+            return JsonResponse({"success": True, "status": status})
+        
+        except json.JSONDecodeError:
+            return JsonResponse({"success": False, "error": "Invalid JSON"}, status=400)
