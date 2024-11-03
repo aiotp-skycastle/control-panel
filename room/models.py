33483425c24ed79b4c589ef4cc3e8cecc7b62d7e
Youngho Cho <dominoyh5@gmail.com>
2024-11-03 17:51:21 +0900

    add: illuminance

--- room/models.py
+++ room/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Illuminance(models.Model):
+    datetime = models.DateTimeField()
+    status = models.FloatField()
+
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d %H:%M:%S') + ': ' + self.status
+
+class Temperature(models.Model):
+    datetime = models.DateTimeField()
+    status = models.FloatField()
+
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d %H:%M:%S') + ': ' + self.status
+
+class Pressure(models.Model):
+    datetime = models.DateTimeField()
+    status = models.FloatField()
+
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d %H:%M:%S') + ': ' + self.status
--- room/views.py
+++ room/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+from .models import Illuminance
+
+@method_decorator(csrf_exempt, name='dispatch')
+class IlluminanceView(View):
+    def get(self, request, *args, **kwargs):
+        recent_illuminance = Illuminance.objects.order_by('-datetime').first()
+        
+        if recent_illuminance:
+            return JsonResponse({
+                "success": True,
+                "datetime": recent_illuminance.datetime,
+                "status": recent_illuminance.status
+            }, status=200)
+        else:
+            return JsonResponse({"success": False, "error": "No data available"}, status=404)
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        status_value = data.get('status')
+        
+        if status_value is not None:
+            try:
+                illuminance = Illuminance(datetime=timezone.now(), status=status_value)
+                illuminance.save()
+                return JsonResponse({"success": True}, status=201)
+            except ValueError:
+                return JsonResponse({"success": False, "error": "Invalid sensor value"}, status=400)
+        
+        return JsonResponse({"success": False, "error": "Status value is required"}, status=400)
