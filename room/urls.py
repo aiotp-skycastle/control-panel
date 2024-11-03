de24a6322efdbc9a2231be7995d1218d3d8c379f
Youngho Cho <dominoyh5@gmail.com>
2024-11-03 18:04:44 +0900

    add: pressure

--- room/urls.py
+++ room/urls.py
@@ -6,4 +6,5 @@ app_name = "room"
 urlpatterns = [
     path("illuminance", views.IlluminanceView.as_view(), name="illuminance"),
     path("temperature", views.TemperatureView.as_view(), name="temperature"),
+    path("pressure", views.PressureView.as_view(), name="pressure"),
 ]
--- room/views.py
+++ room/views.py
@@ -65,3 +65,31 @@ class TemperatureView(View):
                 return JsonResponse({"success": False, "error": "Invalid sensor value"}, status=400)
         
         return JsonResponse({"success": False, "error": "Status value is required"}, status=400)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class PressureView(View):
+    def get(self, request, *args, **kwargs):
+        recent_pressure = Pressure.objects.order_by('-datetime').first()
+        
+        if recent_pressure:
+            return JsonResponse({
+                "success": True,
+                "datetime": recent_pressure.datetime,
+                "status": recent_pressure.status
+            }, status=200)
+        else:
+            return JsonResponse({"success": False, "error": "No data available"}, status=404)
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        status_value = data.get('status')
+        
+        if status_value is not None:
+            try:
+                pressure = Pressure(datetime=timezone.now(), status=status_value)
+                pressure.save()
+                return JsonResponse({"success": True}, status=201)
+            except ValueError:
+                return JsonResponse({"success": False, "error": "Invalid sensor value"}, status=400)
+        
+        return JsonResponse({"success": False, "error": "Status value is required"}, status=400)
