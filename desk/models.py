6eef45691cb6e0b24147bc16ef359a4bf02ee8f3
Youngho Cho <dominoyh5@gmail.com>
2024-11-06 18:28:21 +0900

    add: Servo model

--- desk/models.py
+++ desk/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Desk(models.Model):
     datetime = models.DateTimeField()
 
     def __str__(self):
         return self.datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+class Servo(models.Model):
+    datetime = models.DateTimeField()
+    angle = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(180)]
+    )
+
+    def __str__(self):
+        return f"{self.angle} at {self.datetime}"
