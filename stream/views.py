3c267cde7ba6e032e8239e455a3b74e35660dbe9
Youngho Cho <dominoyh5@gmail.com>
2024-11-22 20:02:24 +0900

    change: func stream view to class

--- stream/views.py
+++ stream/views.py
@@ -1,11 +1,13 @@
 from django.http import FileResponse, HttpResponseNotFound
 from django.conf import settings
+from django.views import View
 import os
 
-def stream(request, filename):
-    file_path = os.path.join(settings.HLS_DIR, filename)
+class StreamView(View):
+    def get(self, request, filename):
+        file_path = os.path.join(settings.HLS_DIR, filename)
 
-    if os.path.exists(file_path):
-        return FileResponse(open(file_path, 'rb'))
-    else:
-        return HttpResponseNotFound("File not found.")
+        if os.path.exists(file_path):
+            return FileResponse(open(file_path, 'rb'))
+        else:
+            return HttpResponseNotFound("File not found.")
