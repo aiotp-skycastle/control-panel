304d2c31e17ca4402ce38b1473707610ab086a4d
Youngho Cho <dominoyh5@gmail.com>
2024-11-23 16:34:02 +0900

    BuzzerView get api

--- buzzer/urls.py
+++ buzzer/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 app_name = "buzzer"
 urlpatterns = [
-    # path("", views.IndexView.as_view(), name="index"),
-    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("status", views.BuzzerView.as_view(), name="buzzer"),
 ]
--- buzzer/views.py
+++ buzzer/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from drf_spectacular.utils import extend_schema
+
+from .models import Buzzer
+from django.utils import timezone
 
 # Create your views here.
+class BuzzerView(APIView):
+    @extend_schema(
+        summary="Retrieve the latest buzzer status",
+        responses={
+            200: {
+                "type": "object",
+                "properties": {
+                    "success": {"type": "boolean"},
+                    "datetime": {"type": "string", "format": "date-time"}
+                },
+            },
+            404: {
+                "type": "object",
+                "properties": {
+                    "success": {"type": "boolean", "example": False},
+                    "error": {"type": "string", "example": "No status found"}
+                },
+            }
+        },
+    )
+    def get(self, request, *args, **kwargs):
+        latest_buzzer = Buzzer.objects.order_by('-datetime').first()
+
+        if latest_buzzer:
+            return Response({
+                "success": True,
+                "datetime": latest_buzzer.datetime.strftime('%Y-%m-%d %H:%M:%S')
+            })
+        else:
+            return Response({
+                "success": False,
+                "error": "No status found"
+            }, status=404)
